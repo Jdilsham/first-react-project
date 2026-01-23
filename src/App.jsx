a7cd216ad12098992b5f83ddee9ae6b8aaa1b1d6
Jdilsham <janithadilsham@gmail.com>
2026-01-24 00:21:42 +0530

    refactor: enhance layout and positioning in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
 
   return (
     <>
-      <div className="h-[700px] w-[700px] border-[5px]">
-        <div className="h-[600px] w-[600px] bg-yellow-300 flex flex-col"> 
+      <div className="h-[700px] w-[700px] border-[5px] flex relative justify-center items-center">
+        <div className="h-[600px] w-[600px] bg-yellow-300 flex flex-col justify-center items-center"> 
 
           <div className='h-[100px] w-[100px] bg-red-500'>
 
@@ -16,12 +16,12 @@ function App() {
 
           </div>
 
-          <div className='h-[100px] w-[100px] bg-pink-500'>
-
+          <div className='h-[100px] w-[100px] bg-pink-500 fixed right-[10px] bottom-[10px] pt-[20px] pl-[30px] '>
+            fixed
           </div>
 
-          <div className='h-[100px] w-[100px] bg-green-500'>
-
+          <div className='h-[100px] w-[100px] bg-green-500 absolute bottom-[10px] right-[10px]'>
+            absolute
           </div>
 
           <div className='h-[100px] w-[100px] bg-gray-500'>
