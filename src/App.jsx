dacfdd8ca114113306390bf70503ae1cfecc9a96
Jdilsham <janithadilsham@gmail.com>
2026-01-29 22:00:07 +0530

    feat: add react-router-dom for routing and update App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import ProductCard from './components/productCard'
 
@@ -5,33 +6,16 @@ function App() {
 
   return (
     <>
-      <div className="h-[700px] w-[700px] border-[5px] flex relative justify-center items-center">
-        <div className="h-[600px] w-[600px] bg-yellow-300 flex flex-col justify-center items-center"> 
+      <BrowserRouter>
+        <div className='w-full h-[100vh] bg-sky-500'>
 
-          <div className='h-[100px] w-[100px] bg-red-500'>
-
-          </div>
-
-          <div className='h-[100px] w-[100px] bg-blue-500 m-[20px]'>
-
-          </div>
-
-          <div className='h-[100px] w-[100px] bg-pink-500 fixed right-[10px] bottom-[10px] pt-[20px] pl-[30px] '>
-            fixed
-          </div>
-
-          <div className='h-[100px] w-[100px] bg-green-500 absolute bottom-[10px] right-[10px]'>
-            absolute
-          </div>
-
-          <div className='h-[100px] w-[100px] bg-gray-500'>
-            
-
-          </div>
+          <Routes path="/">
+              <Route path="/" element={<h1>Home Page</h1>}/>
+              <Route path="/admin" element={<h1>Admin Page</h1>}/>
+          </Routes>
 
         </div>
-        
-      </div>
+      </BrowserRouter>
     </>
   )
 }
