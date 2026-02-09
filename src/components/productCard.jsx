d1943b34a4ff929a8c2bbeebbc76a53c02118ff7
Jdilsham <janithadilsham@gmail.com>
2026-02-09 20:59:01 +0530

    product card

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,76 +1,87 @@
-export default function ProductCard({ product }) {
-  return (
+export default function ProductCard({product}){
+  return(
+
     <div className="
-      w-[240px]
-      bg-white
-      rounded-xl
-      border border-gray-200
-      shadow-sm
-      hover:shadow-lg
-      transition
-    ">
-
-      {/* Image */}
-      <div className="relative h-[220px] bg-gray-50 rounded-t-xl overflow-hidden">
-        <img
-          src={product.images?.[0]}
-          alt={product.name}
-          className="w-full h-full object-contain p-4"
-        />
-
-        {product.stock > 0 && (
-          <span className="
-            absolute top-3 left-3
-            bg-gray-500
-            text-white
-            text-xs
-            px-2 py-1
-            rounded-md
-          ">
-            In Stock
-          </span>
-        )}
-      </div>
-
-      {/* Content */}
-      <div className="p-4 space-y-2">
-        <h3 className="text-sm font-medium text-gray-900 line-clamp-2">
-          {product.name}
-        </h3>
-
-        {/* Price */}
-        <div>
-          <span className="text-lg font-semibold text-primary">
-            Rs. {product.price}
-          </span>
-          {product.labeled_price && (
-            <span className="ml-2 text-sm text-gray-400 line-through">
-              Rs. {product.labeled_price}
-            </span>
-          )}
+          w-[260px]
+          bg-white
+          rounded-xl
+          border border-gray-200
+          shadow-sm
+          hover:shadow-lg
+          hover:scale-105
+          transition
+          duration-400
+      ">
+    
+        <div className="relative w-full h-[260px] bg-gray-50 rounded-t-xl overflow-hidden">
+          <img 
+            src={product.images?.[0]}
+            alt={product.name}
+            className="w-full h-full object-cover p-1 pt-5"
+          />
+          
+          {
+            product.stock > 0 ? (
+              <span className="
+                absolute top-3 left-3
+                bg-gray-500
+                text-white
+                text-xs
+                px-2 py-1
+                rounded-md
+              ">
+                In Stock
+              </span>
+            ):(
+              <span className="
+                absolute top-3 left-3
+                bg-gray-500
+                text-white
+                text-xs
+                px-2 py-1
+                rounded-md
+              ">
+                Out of Stock
+              </span>
+            )
+          }
         </div>
 
-        {/* Button */}
-       <button
-            className="
-                mt-3
-                w-full
-                py-2
-                rounded-lg
-                text-sm
-                font-medium
-                border-2
-                border-primary
-                text-green-600
-                hover:bg-primary
-                hover:text-white
-                transition
-                duration-200
-            "
-            >
+        <div className="p-4 space-y-2">
+          <h3 className="text-lg font-semibold text-text">
+            {product.name}
+          </h3>
+
+          <div>
+            <span className="text-lg text-primary font-semibold">
+              Rs.{product.price}
+            </span>
+            {product.labeled_price && (
+              <span className="ml-3 text-sm text-gray-400 line-through">
+                Rs. {product.labeled_price}
+              </span>
+            )}
+          </div>
+
+          <button className="
+            mt-3
+            w-full
+            py-2
+            border-2
+            rounded-md
+            border-primary
+            text-primary
+            hover:bg-primary
+            hover:text-white
+            transition
+            duration-400
+            
+          ">
             View Details
-        </button>
+          </button>
+        </div>
+
       </div>
-    </div>
   );
+  
 }
--- src/pages/productPage.jsx
+++ src/pages/productPage.jsx
@@ -35,7 +35,7 @@ export default function ProductPage(){
                 sm:grid-cols-3
                 md:grid-cols-4
                 lg:grid-cols-5
-                gap-8
+                gap-50
                 ">
                 {products.map((item) => (
                     <ProductCard key={item.product_id} product={item} />
